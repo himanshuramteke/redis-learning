b5e1c770a8fd9ab35d4248383ddbc4728b00da85
Himanshu Ramteke <himanshuramteke007@gmail.com>
2026-06-03 12:48:26 +0530

    learn about bullmq using email-queue

---
+++ 07_order_confirmation_job_with_bullmq/src/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import { emailQueue } from "./queue";
+
+const app = express();
+app.use(express.json());
+
+app.post("/welcome-emails", async (req, res) => {
+  const job = emailQueue.add(
+    "send-welcome-email",
+    {
+      to: req.body.to,
+      name: req.body.name || "Learner",
+    },
+    {
+      attempts: 3,
+      backoff: {
+        type: "exponential",
+        delay: 1000,
+      },
+    },
+  );
+  res.json({
+    message: "Welcome email job has been added to the queue",
+    jobId: job,
+  });
+});
+
+app.listen(3000, () => {
+  console.log(`Server is running on http://localhost:3000`);
+});
---
+++ 07_order_confirmation_job_with_bullmq/src/worker.js
@@ -0,0 +1,19 @@
+import { Worker } from "bullmq";
+import { connection } from "./queue";
+
+const worker = new Worker(
+  "emails",
+  async (job) => {
+    console.log("Processing email job...", job.id, job.name, job.data);
+    await new Promise((resolve) => setTimeout(resolve, 1500));
+  },
+  { connection },
+);
+
+worker.on("completed", (job) => {
+  console.log("Job completed!", job.id, job.name, job.data);
+});
+
+worker.on("failed", (job, err) => {
+  console.log("Job failed!!", job.id, job.name, job.data);
+});
